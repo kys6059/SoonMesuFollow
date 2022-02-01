2f4654365d19d6c8b1b0dd1331103afdddc1cfbc
youngsoo.kim <f92887@hanmail.net>
2022-02-01 11:05:43 +0900

    grid1을 초기화하고 data 입력까지 끝남

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -23,28 +23,43 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.btn_RealTimeStart = New System.Windows.Forms.Button()
+        Me.grid1 = New System.Windows.Forms.DataGridView()
+        CType(Me.grid1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'btn_RealTimeStart
         '
-        Me.btn_RealTimeStart.Location = New System.Drawing.Point(74, 39)
+        Me.btn_RealTimeStart.Location = New System.Drawing.Point(86, 36)
         Me.btn_RealTimeStart.Name = "btn_RealTimeStart"
-        Me.btn_RealTimeStart.Size = New System.Drawing.Size(75, 23)
+        Me.btn_RealTimeStart.Size = New System.Drawing.Size(87, 21)
         Me.btn_RealTimeStart.TabIndex = 0
         Me.btn_RealTimeStart.Text = "대신 연결"
         Me.btn_RealTimeStart.UseVisualStyleBackColor = True
         '
+        'grid1
+        '
+        Me.grid1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.grid1.Location = New System.Drawing.Point(12, 108)
+        Me.grid1.Name = "grid1"
+        Me.grid1.ReadOnly = True
+        Me.grid1.RowTemplate.Height = 23
+        Me.grid1.Size = New System.Drawing.Size(1221, 836)
+        Me.grid1.TabIndex = 1
+        '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(2025, 1036)
+        Me.ClientSize = New System.Drawing.Size(2362, 956)
+        Me.Controls.Add(Me.grid1)
         Me.Controls.Add(Me.btn_RealTimeStart)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.grid1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents btn_RealTimeStart As Button
+    Friend WithEvents grid1 As DataGridView
 End Class
--- Form1.vb
+++ Form1.vb
@@ -34,9 +34,12 @@ Public Class Form1
             Loop
 
             If TotalCount > 0 Then
+
                 SetTimeDataForData() '미리 data구조체에 시간을 다 입력해 놓는다. 카운트만큼
 
                 GetAllData()
+
+                RedrawAll()
             End If
 
 
@@ -45,4 +48,101 @@ Public Class Form1
         Return False
 
     End Function
+
+    Private Sub RedrawAll()
+
+        If currentIndex > 0 Then
+            'grid1.Visible = False
+            InitFirstGrid()
+            DrawGrid1Data()
+        End If
+
+    End Sub
+
+    Private Sub InitFirstGrid()
+        Dim jongMok As String
+        Dim i, j As Integer
+
+        '전체 크기 지정
+        grid1.ColumnCount = TotalCount * 10 + 1
+        grid1.RowCount = timeIndex + 3
+
+        grid1.Columns(0).HeaderText = "시간"
+        grid1.Columns(TotalCount * 10).HeaderText = "시간"
+        grid1.Columns(0).Width = 40
+        grid1.Columns(TotalCount * 10).Width = 40
+
+        grid1.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+
+        '0번 컬럼과 마지막 컬럼에 시간을 넣고 5번열에는 종목행사가를 넣는다
+        For j = 0 To timeIndex + 1
+            grid1.Rows(j).Height = 21 '전체 Row 높이 지정
+            grid1.Rows(j).Cells(0).Value = Data(0).ctime(j)
+            grid1.Rows(j).Cells(TotalCount * 10).Value = Data(0).ctime(j)
+
+        Next
+
+        For i = 0 To TotalCount - 1
+            jongMok = Data(i).HangSaGa
+
+            grid1.Columns(i * 10 + 1).HeaderText = "시"
+            grid1.Columns(i * 10 + 2).HeaderText = "고"
+            grid1.Columns(i * 10 + 3).HeaderText = "저"
+            grid1.Columns(i * 10 + 4).HeaderText = "종"
+            grid1.Columns(i * 10 + 5).HeaderText = jongMok
+            grid1.Columns(i * 10 + 6).HeaderText = "시"
+            grid1.Columns(i * 10 + 7).HeaderText = "고"
+            grid1.Columns(i * 10 + 8).HeaderText = "저"
+            grid1.Columns(i * 10 + 9).HeaderText = "종"
+
+            grid1.Columns(i * 10 + 1).Width = 50
+            grid1.Columns(i * 10 + 2).Width = 50
+            grid1.Columns(i * 10 + 3).Width = 50
+            grid1.Columns(i * 10 + 4).Width = 50
+            grid1.Columns(i * 10 + 5).Width = 50
+            grid1.Columns(i * 10 + 6).Width = 50
+            grid1.Columns(i * 10 + 7).Width = 50
+            grid1.Columns(i * 10 + 8).Width = 50
+            grid1.Columns(i * 10 + 9).Width = 50
+
+        Next
+
+        For i = 0 To TotalCount - 2
+            grid1.Columns(i * 10 + 10).Width = 10
+        Next
+
+        grid1.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+    End Sub
+
+    Private Sub DrawGrid1Data()
+        Dim i, j As Integer
+
+        For i = 0 To TotalCount - 1
+            For j = 0 To currentIndex + 1
+
+                If Val(Data(i).ctime(j)) > 0 Then
+
+                    grid1.Rows(j).Cells(i * 10 + 5).Value = Data(i).HangSaGa
+
+                    If Data(i).price(0, j, 0) > 0 Then  '시가가 0보다 크면 입력, 즉 4개다 데이터가 있을 때만 입력 - 콜
+
+                        grid1.Rows(j).Cells(i * 10 + 1).Value = Data(i).price(0, j, 0)
+                        grid1.Rows(j).Cells(i * 10 + 2).Value = Data(i).price(0, j, 1)
+                        grid1.Rows(j).Cells(i * 10 + 3).Value = Data(i).price(0, j, 2)
+                        grid1.Rows(j).Cells(i * 10 + 4).Value = Data(i).price(0, j, 3)
+                    End If
+
+                    If Data(i).price(1, j, 0) > 0 Then  '시가가 0보다 크면 입력, 즉 4개다 데이터가 있을 때만 입력 - 풋
+                        grid1.Rows(j).Cells(i * 10 + 6).Value = Data(i).price(1, j, 0)
+                        grid1.Rows(j).Cells(i * 10 + 7).Value = Data(i).price(1, j, 1)
+                        grid1.Rows(j).Cells(i * 10 + 8).Value = Data(i).price(1, j, 2)
+                        grid1.Rows(j).Cells(i * 10 + 9).Value = Data(i).price(1, j, 3)
+                    End If
+
+                End If
+            Next
+        Next
+    End Sub
+
+
 End Class
