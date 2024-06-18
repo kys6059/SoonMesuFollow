d0bc241b2650614427f6c464bd13905da9393e37
Kim YoungSoo <f92887@hanmail.net>
2024-06-18 15:10:29 +0900

    10시20분부터로 변경

--- Algorithm_SoonMeSu.vb
+++ Algorithm_SoonMeSu.vb
@@ -2013,7 +2013,7 @@ Module Algorithm_SoonMeSu
 
             'If Math.Abs(투자그레이드) = 1 Then 진짜최종투자금액 = 진짜최종투자금액 * 0.9
 
-            If Val(순매수리스트(currentIndex_순매수).sTime) >= 100000 And Val(순매수리스트(currentIndex_순매수).sTime) <= 103000 Then   '10시30분 전에는 켈리지수가 않좋아서 70%만 투자한다
+            If Val(순매수리스트(currentIndex_순매수).sTime) >= 100000 And Val(순매수리스트(currentIndex_순매수).sTime) < 102000 Then   '10시30분 전에는 켈리지수가 않좋아서 70%만 투자한다
                 진짜최종투자금액 = 진짜최종투자금액 * 0.7
             End If
 
